c52d6c5144a11c8b37b4086cc797d41a1d29c497
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-25 22:02:19 +0400

    Task2 added(Switch Case)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 // Task1
-var number = Number(prompt('Enter number..'));
-var percent = number*35/100;
-if(percent <= 1000){
-    console.log('The number satisfies the given condition.')
-}else{
-    console.log('The number does not meet the given condition')
+// var number = Number(prompt('Enter number..'));
+// var percent = number*35/100;
+// if(percent <= 1000){
+//     console.log('The number satisfies the given condition.')
+// }else{
+//     console.log('The number does not meet the given condition')
+// }
+
+// Task2
+
+var num1 = Number(prompt("Enter num1.."));
+var num2 = Number(prompt("Enter num2.."));
+var simvol = prompt("Enter simvol");
+
+switch (simvol) {
+  case "+": {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+    break;
+  }
+  case "-": {
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+    break;
+  }
+  case "*": {
+    console.log(`${num1} * ${num2} = ${num1 * num2}`);
+    break;
+  }
+  case "/": {
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+    break;
+  }
+  default: {
+    console.log("Please, enter the character correctly..");
+  }
 }
