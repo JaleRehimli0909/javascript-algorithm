7bbdbfe7d4bd87e9ee0f00cc28359c38cc31fb2e
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-25 22:12:21 +0400

    Task3 added(Sum of numbers)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-// Task1
-// var number = Number(prompt('Enter number..'));
-// var percent = number*35/100;
-// if(percent <= 1000){
-//     console.log('The number satisfies the given condition.')
-// }else{
-//     console.log('The number does not meet the given condition')
-// }
+/*
+Task1
+var number = Number(prompt('Enter number..'));
+var percent = number*35/100;
+if(percent <= 1000){
+    console.log('The number satisfies the given condition.')
+}else{
+    console.log('The number does not meet the given condition')
+}
+*/
 
-// Task2
-
+/*
+Task2
 var num1 = Number(prompt("Enter num1.."));
 var num2 = Number(prompt("Enter num2.."));
 var simvol = prompt("Enter simvol");
@@ -34,3 +36,22 @@ switch (simvol) {
     console.log("Please, enter the character correctly..");
   }
 }
+*/
+
+/* Task3
+
+var number = Number(prompt('Enter number..'));
+
+Usul1:
+var sum = (1+number)*number / 2;
+console.log(sum);
+
+
+Usul2:
+var sum = 0;
+for(let i = 0; i <=number; i++){
+    sum+=i
+}
+console.log(sum)
+
+*/
