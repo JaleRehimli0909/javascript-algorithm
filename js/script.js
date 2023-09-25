ad032ece4cda01b9c96ae57048eee08b294810b9
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-25 21:40:01 +0400

    Task1 added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Task1
+var number = Number(prompt('Enter number..'));
+var percent = number*35/100;
+if(percent <= 1000){
+    console.log('The number satisfies the given condition.')
+}else{
+    console.log('The number does not meet the given condition')
+}
